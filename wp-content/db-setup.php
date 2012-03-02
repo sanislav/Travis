c2c47f10630f14793c2cc3e14aae14dbc949944c
Sabin Sanislav <sanislav@gmail.com>
2012-03-02 18:54:44 +0200

    select test

--- wp-content/db-setup.php
+++ wp-content/db-setup.php
@@ -1,8 +1,8 @@
 <?php 
 
-$query = file_get_contents( './wp-content/wp_travis.sql' );
+$query = file_get_contents( 'wp_travis.sql' );
 
-$link = mysql_connect('localhost', 'root', '');
+$link = mysql_connect('localhost', 'root', 'anaaremere');
 
 if (!$link) {
     die('Could not connect: ' . mysql_error());
@@ -13,7 +13,6 @@ mysql_select_db("wp_travis", $link);
 mysql_query( $query );
 
 $select_test = mysql_query( "SELECT * FROM `wp_users`" );
-
 $select_test = mysql_fetch_array( $select_test );
 
 echo $select_test['user_login']; die;
