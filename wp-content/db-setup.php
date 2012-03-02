205749ed42bf2b55c6f78676cbd6cc6abce94dc0
Sabin Sanislav <sanislav@gmail.com>
2012-03-02 18:08:13 +0200

    add db setup

---
+++ wp-content/db-setup.php
@@ -0,0 +1,19 @@
+<?php 
+
+$query = file_get_contents( './wp-content/wp_travis.sql' );
+
+$link = mysql_connect('localhost', 'root', '');
+
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+
+mysql_select_db("wp_travis", $link);
+
+mysql_query( $query );
+
+$select_test = mysql_query( "SELECT * FROM wp_users WHERE ID = 1" );
+var_dump($select_test); die;
+
+mysql_close($link);
+?>
